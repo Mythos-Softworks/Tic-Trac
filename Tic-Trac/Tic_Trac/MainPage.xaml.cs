df23fa5570b92426b4948de9b59695d28ddf307f
Austin Rotert <austin.rotert@mines.sdsmt.edu>
2017-01-23 16:39:52 -0700

    Add a grid with buttons to the main page
    Added a grid with tick-offset buttons, as well as a
    button for clear, add, and hold. The ListView for the characters
    is partially implemented.

--- Tic-Trac/Tic_Trac/MainPage.xaml.cs
+++ Tic-Trac/Tic_Trac/MainPage.xaml.cs
@@ -9,9 +9,60 @@ namespace Tic_Trac
 {
     public partial class MainPage : ContentPage
     {
+#warning Change to cell type when finished
+        private List<string> _CharacterList = new List<string>();
+
         public MainPage()
         {
+            BindingContext = this;
             InitializeComponent();
+
+            _CharacterList.Add("a");
+            _CharacterList.Add("b");
+            _CharacterList.Add("c");
+            _CharacterList.Add("d");
+            _CharacterList.Add("a");
+            _CharacterList.Add("b");
+            _CharacterList.Add("c");
+            _CharacterList.Add("d");
+            _CharacterList.Add("a");
+            _CharacterList.Add("b");
+            _CharacterList.Add("c");
+            _CharacterList.Add("d");
+        }
+
+        private void btnOffset_Clicked(object sender, EventArgs args)
+        {
+            int offset;
+            Button btn = sender as Button;
+
+            if(int.TryParse(btn.Text, out offset))
+            {
+                ApplyOffset(offset);
+            }
+        }
+
+        private void btnHold_Clicked(object sender, EventArgs args)
+        {
+#warning    Find next person in list, get their offset, offset to them
+            int offset = -1;
+
+            ApplyOffset(offset);
+        }
+
+        private void btnAddChar_Clicked(object sender, EventArgs args)
+        {
+#warning    Add character to list
+        }
+
+        private void btnClear_Clicked(object sender, EventArgs args)
+        {
+#warning    Clear list of characters
+        }
+
+        private void ApplyOffset(int offset)
+        {
+
         }
     }
 }
